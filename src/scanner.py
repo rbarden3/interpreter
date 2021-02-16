b1bcfbd9d3c8e43e47e605eeceb880afb47882bb
jamesrivett <jamesrivett@users.noreply.github.com>
2021-02-15 22:52:26 -0500

    Started Scanner

--- src/scanner.py
+++ src/scanner.py
@@ -5,4 +5,23 @@
 # Instructor:   Deepa Muralidhar
 # Project:  Deliverable 1 Scanner - Python
 
+import re              # import re to match regx
 from tokens import *   # import the basic subset to be used
+
+class Token:
+
+        # Constructor for token class
+        def __init__(self, type: Tokens, lexeme: str, pos: tuple):
+            self.type = type
+            self.lexeme = lexeme
+            self.pos = pos
+        
+        # Returns Token as a string
+        def __str__(self):
+            str_form = ("Line: {:<3} Column: {:<4} Token: {:<30} Lexemme: {:<20}")
+            return str_form.format(self.pos[0], self.pos[1], self.type, repr(self.lexeme))
+
+
+class ScannerError(Exception):
+    def __init__(self, pos: tuple):
+        self.pos = pos
