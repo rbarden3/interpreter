b3d1a166d59fb6e21656f51565a444901c64578a
Raleigh Barden <rbardenksu@gmail.com>
2021-02-15 21:59:32 -0500

    Add scanner.py

---
+++ src/scanner.py
@@ -0,0 +1,8 @@
+
+# Class:       CS 4308 Section 2
+# Term:        Spring 2021
+# Name:        Raleigh Barden, James Rivett, Barrett Calhoun
+# Instructor:   Deepa Muralidhar
+# Project:  Deliverable 1 Scanner - Python
+
+from tokens import *   # import the basic subset to be used
